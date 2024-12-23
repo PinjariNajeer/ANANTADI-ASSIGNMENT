356599ce9857156a14d47cc7d47b9c32215ca6b6
PinjariNajeer <najeerb038@example.com>
2024-12-23 13:11:35 +0530

    intializing app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
-import { Route, Router } from 'react-router-dom'
-import Home from './pages/Home'
-import Search from './pages/Search'
-import Details from './pages/Details'
 
 const App = () => {
   return (
-    <div>
-      <>
-       <Home />
-       <Search />
-       <Details />
-      </>
-    </div>
+    <div>App</div>
   )
 }
 
